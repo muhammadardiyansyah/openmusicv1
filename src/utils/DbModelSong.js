496920088048c902c4ff3d584ebef11523457a60
Muhammad Ardiyansyah <muhammadardiyansyah46@gmail.com>
2022-05-05 01:02:02 +0800

    initial commit

---
+++ src/utils/DbModelSong.js
@@ -0,0 +1,21 @@
+const mapDBModelSong = ({
+  id,
+  name,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id,
+}) => ({
+  id,
+  name,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = { mapDBModelSong };
